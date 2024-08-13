f03f2ef2725d2ad95dc5b1dc3701305f9f3d5632
lelesanmir <leonardodossantosmiranda@gmail.com>
2024-08-12 21:53:52 -0300

    Projeto iniciado.

---
+++ src/main/java/com/leonardo/servicoDeSuporte/ServicoDeSuporteApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.servicoDeSuporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeSuporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeSuporteApplication.class, args);
+	}
+
+}
